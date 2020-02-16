cad26ecbaf73ecd478d7c445b21d182259637f62
Melissa Sperry <sperry.mel@gmail.com>
2020-02-16 04:28:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,22 @@
-  def badge_maker (name)
- "Hello, my name is #{name}."
+def badge_maker(name)
+return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator (attendees)
- attendees.collect do |name| badge_maker (name)
- end
+def batch_badge_creator(array)
+  nuarray = []
+  array.each do |name|
+    nuarray.push("Hello, my name is #{name}.")
+  end
+  return nuarray
 end
+
+def assign_rooms(array)
+  nuarray = []
+  counter = 1
+  array.each do |name|
+    nuarray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  return nuarray
+end
+
