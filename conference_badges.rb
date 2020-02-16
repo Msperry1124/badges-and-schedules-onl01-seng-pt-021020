3bb4944a5d97e88deb15a74ae533f8e0307da966
Melissa Sperry <sperry.mel@gmail.com>
2020-02-16 03:57:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+  def badge_maker (name)
+ "Hello, my name is #{name}."
+end# Write your code here.
+def batch_badge_creator (attendees)
+ attendees.collect do |name| badge_maker (name)
+ end
+end
